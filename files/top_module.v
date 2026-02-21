e4ea2804745d0f1decc66968e59904d8e133d5dd
weslee <142736134+wesleejacop@users.noreply.github.com>
2026-02-21 17:25:03 +0530

    Clean up header comments in top_module.v
    Removed header comments from top_module.v

--- files/top_module.v
+++ files/top_module.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.01.2026 16:19:14
-// Design Name: 
-// Module Name: top_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 
